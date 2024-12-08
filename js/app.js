9bc57c2004e62df84560b9c277e82e77a54c2aa0
Ed <ruscoedo@gmail.com>
2024-12-08 12:01:19 +0000

    Modified background, removed lines, made more dense, adds more on click. Removed useless particles.json, edit app.js instead

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
+    number: { value: 80, density: { enable: true, value_area: 400 } },
     color: { value: "#ffffff" },
     shape: {
       type: "image",
@@ -8,7 +8,7 @@ particlesJS("particles-js", {
       polygon: { nb_sides: 5 },
       image: {
         src:
-          "https://media.discordapp.net/attachments/505073609379938337/1315098008760614912/bruh-removebg-preview.png?ex=67562bf6&is=6754da76&hm=b574fd33b91d12579a5610f3c5ed3056f4c2ef7f617c85b374516c0c9838b188&=&format=webp&quality=lossless&width=433&height=701",
+          "bruh.png",
         width: 1000,
         height: 1000
       }
@@ -19,12 +19,12 @@ particlesJS("particles-js", {
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
     size: {
-      value: 39.98400639744104,
+      value: 40,
       random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+      anim: { enable: false, speed: 40, size_min: 10, sync: false }
     },
     line_linked: {
-      enable: true,
+      enable: false,
       distance: 150,
       color: "#ffffff",
       opacity: 0.4,
@@ -32,7 +32,7 @@ particlesJS("particles-js", {
     },
     move: {
       enable: true,
-      speed: 6,
+      speed: 10,
       direction: "none",
       random: false,
       straight: false,
@@ -52,7 +52,7 @@ particlesJS("particles-js", {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
       repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
+      push: { particles_nb: 12 },
       remove: { particles_nb: 2 }
     }
   },
